3f759222a5683ccac02f03ac4bfef61fb901b3d4
mutasem <motasemimad10@gmail.com>
2021-09-16 18:54:47 +0300

    connecting to mongoDB

---
+++ config/dbConnect.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const dbConnect = () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  const URI = process.env.DB_LOCAL_URI;
+  mongoose.connect(URI, { useUnifiedTopology: true });
+
+  const connection = mongoose.connection;
+  connection.once('open', () => {
+    console.log('Connected to local database.');
+  });
+
+  //   mongoose
+  //     .connect(process.env.DB_LOCAL_URI, {
+  //       useNewUrlParser: true,
+  //       useUnifiedTopology: true,
+  //       useFindAndModify: false,
+  //       useCreateIndex: true,
+  //     })
+  //     .then((con) => console.log('Connected to local database.'));
+};
+
+export default dbConnect;
--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,7 +1,10 @@
 import nc from 'next-connect';
+import dbConnect from '../../../config/dbConnect';
 import { allRooms } from '../../../controllers/roomControllers';
 
 const handler = nc();
+
+dbConnect();
 
 handler.get(allRooms);
 
