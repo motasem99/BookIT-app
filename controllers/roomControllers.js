34ad72f8241332796fba922b49ade48689016c52
mutasem <motasemimad10@gmail.com>
2021-09-18 13:37:57 +0300

    fetch all rooms

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,10 +1,20 @@
 import Room from '../models/room';
 
-const allRooms = (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'All Rooms',
-  });
+const allRooms = async (req, res) => {
+  try {
+    const rooms = await Room.find();
+
+    res.status(200).json({
+      success: true,
+      count: rooms.length,
+      rooms,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 // Create new room => /api/rooms
