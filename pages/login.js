66a90f9a772ddcda9ea4a4ecbe39ca1f564719c2
mutasem <motasemimad10@gmail.com>
2021-09-23 15:21:16 +0300

    login user component

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import Login from '../components/auth/Login';
+import Layout from '../components/layout/Layout';
+
+const loginPage = () => {
+  return (
+    <Layout title='Login'>
+      <Login />
+    </Layout>
+  );
+};
+
+export default loginPage;
