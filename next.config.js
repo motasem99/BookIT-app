20f2d676ecfd84a409199f941cae8795c0c47f10
mutasem <motasemimad10@gmail.com>
2021-09-23 16:18:07 +0300

    adding cloudinary for images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
   reactStrictMode: true,
   env: {
     DB_LOCAL_URI: 'mongodb://localhost:27017/bookit',
+
+    CLOUDINARY_CLOUD_NAME: 'bookit-app-mutasem',
+    CLOUDINARY_API_KEY: '177456338558743',
+    CLOUDINARY_API_SECRET: 'kQ9Ny3_7F2y-pjfHfDQOXQ5KYcI',
   },
   images: {
     domains: ['res.cloudinary.com', 'a0.muscache.com'],
