3f77478694e69601380438d8e1c45055b43bb5eb
mutasem <motasemimad10@gmail.com>
2021-09-25 15:57:08 +0300

    update user profile endpoint

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -51,4 +51,42 @@ const currentUserProfile = catchAsyncErrors(async (req, res) => {
   });
 });
 
-export { registerUser, currentUserProfile };
+// Update user profile => /api/me/update
+
+const updateProfile = catchAsyncErrors(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name;
+    user.email = req.body.email;
+
+    if (req.body.password) user.password = req.body.password;
+  }
+
+  // update avatar
+  if (req.body.avatar !== '') {
+    const image_id = user.avatar.public_id;
+
+    // delete user previous image/avatar
+    await cloudinary.v2.uploader.destroy(image_id);
+
+    const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+      folder: 'bookit/avatars',
+      width: '150',
+      crop: 'scale',
+    });
+
+    user.avatar = {
+      public_id: result.public_id,
+      url: result.secure_url,
+    };
+  }
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+export { registerUser, currentUserProfile, updateProfile };
