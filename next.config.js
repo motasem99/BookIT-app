91dbce0f8a8211ef32c854b4694f80990a144351
mutasem <motasemimad10@gmail.com>
2021-09-29 14:10:03 +0300

    setup api key for stripe payment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ module.exports = {
   env: {
     DB_LOCAL_URI: 'mongodb://localhost:27017/bookit',
 
+    STRIPE_API_KEY:
+      'pk_test_51Jf0YsIZdnwzeUymHZyhBuSkmhuAiTpp9f8wryGoDbqULC7yBjEoj7uOZOdbcZo9oy6pE2VwxVnbePDXUJaXDoYV00DNMQTf4k',
+    STRIPE_SECRET_KEY:
+      'sk_test_51Jf0YsIZdnwzeUymlpVKBemaAq3x5UZ5fSM4k2pUbi3izp8hqKoBuI0xMii5nzkfIqzFb93793YZUEmYxe4PIHLP00U08fuai4',
+
     CLOUDINARY_CLOUD_NAME: 'bookit-app-mutasem',
     CLOUDINARY_API_KEY: '177456338558743',
     CLOUDINARY_API_SECRET: 'kQ9Ny3_7F2y-pjfHfDQOXQ5KYcI',
