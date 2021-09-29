07d0f2b50ff72fc658faa7ca8d77e01882b0e063
mutasem <motasemimad10@gmail.com>
2021-09-29 17:50:20 +0300

    new review endpoint

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -111,4 +111,51 @@ const deleteRoom = async (req, res) => {
   }
 };
 
-export { allRooms, newRoom, getSingleRoom, updateRoom, deleteRoom };
+// Create new review  => /api/review
+const createRoomReview = async (req, res) => {
+  const { rating, comment, roomId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const room = await Room.findById(roomId);
+
+  const isReviewed = room.reviews.find(
+    (r) => r.user.toString() === req.user._id.toString()
+  );
+
+  if (isReviewed) {
+    room.reviews.forEach((review) => {
+      if (review.user.toString() === req.user._id.toString()) {
+        review.comment = comment;
+        review.rating = rating;
+      }
+    });
+  } else {
+    room.reviews.push(review);
+    room.numOfReviews = room.reviews.length;
+  }
+
+  room.ratings =
+    room.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    room.reviews.length;
+
+  await room.save({ validateBeforeSave: false });
+
+  res.status(400).json({
+    success: false,
+  });
+};
+
+export {
+  allRooms,
+  newRoom,
+  getSingleRoom,
+  updateRoom,
+  deleteRoom,
+  createRoomReview,
+};
